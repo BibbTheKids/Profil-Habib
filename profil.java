e981beec06822157bfcce193382122fc227bf173
BibbTheKids <148925792+BibbTheKids@users.noreply.github.com>
2024-09-17 19:09:37 +0700

    Add files via upload

---
+++ profil.java
@@ -0,0 +1,13 @@
+package habib;
+
+public class profil {
+    public static void main(String[] args) {
+        String Nama = "Hariri Hasnul Habib Rambe";
+        String NIM = "12350113075";
+        String Hobi = "Olahraga dan game";
+
+        System.out.println(Nama);
+        System.out.println(NIM);
+        System.out.println(Hobi);
+    }
+}
